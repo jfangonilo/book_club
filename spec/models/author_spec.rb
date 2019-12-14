b31664bb990c7d128afd2c67d22b9a14945dc88f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-14 11:06:12 -0700

    Add spec for average page count

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,4 +9,16 @@ describe Author do
     it {should have_many :book_authors}
     it {should have_many(:books).through(:book_authors)}
   end
+
+  describe "instance methods" do
+    it ".average_page_count" do
+      author = create(:author)
+      book_1 = create(:book, pages: 100)
+      book_2 = create(:book, pages: 1000)
+      book_3 = create(:book, pages: 350)
+      author.books << [book_1, book_2, book_3]
+
+      expect(author.average_page_count).to_be between(483, 484)
+    end
+  end
 end
